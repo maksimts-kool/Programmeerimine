0b3a9ca316bd5f8c83c9800b799d0b119f33f6b3
Maksim Tsikvasvili <maksimtsitkool@gmail.com>
2025-09-04 13:14:59 +0300

    Create Tund1

---
+++ Tund1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Tund1;
+
+namespace Programmeerimine
+{
+    internal class StartClass
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            int a = 1000;
+            /* Console.WriteLine("Tere tulemast!");
+            string tekst = Console.ReadLine();
+            Console.WriteLine($"Teie sisestatud tekst on: {tekst}");
+            char taht = 'A';
+            Console.Write($"Esimene arv on {a}, siseta b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Esimene arv on {0}, sisesta b: {1}, summa: {2}", a, b, a + b);
+            Console.WriteLine("Ujukomaarvud");
+            double d = double.Parse(Console.ReadLine());
+            Console.WriteLine(d);
+            float f = float.Parse(Console.ReadLine());
+            Console.WriteLine(f); */
+
+            bool t = true;
+            Random rnd = new Random();
+            a = rnd.Next(1, 11);
+            Console.WriteLine(a);
+
+            float vastus = osa2.Kalkulaator(5, 2);
+            Console.WriteLine($"Kalkulaatori vastus: {vastus}");
+        }
+    }
+}
---
+++ Tund1/osa2.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Tund1;
+
+public class osa2
+{
+    public static float Kalkulaator(float a, float b)
+    {
+        float vastus = a + b;
+        return vastus;
+    }
+}
