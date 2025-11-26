b5f2ec7876009b62e83106f69203e37c10807362
maksimts-kool <maksimtsitkool@gmail.com>
2025-11-26 18:05:42 +0200

    Update Tund9

--- Tund9/Pood.Designer.cs
+++ Tund9/Pood.Designer.cs
@@ -41,6 +41,8 @@ partial class Pood
         this.Button10 = new System.Windows.Forms.Button();
         this.DataGridView11 = new System.Windows.Forms.DataGridView();
         this.PictureBox13 = new System.Windows.Forms.PictureBox();
+        this.Button13 = new System.Windows.Forms.Button();
+        this.ComboLang = new System.Windows.Forms.ComboBox();
         ((System.ComponentModel.ISupportInitialize)(this.DataGridView11)).BeginInit();
         ((System.ComponentModel.ISupportInitialize)(this.PictureBox13)).BeginInit();
         this.SuspendLayout();
@@ -82,21 +84,18 @@ partial class Pood
         //
         // TextBox4
         //
-        this.TextBox4.Text = "TextBox4";
         this.TextBox4.Location = new System.Drawing.Point(128, 32);
         this.TextBox4.Size = new System.Drawing.Size(120, 23);
         this.TextBox4.TabIndex = 4;
         //
         // TextBox5
         //
-        this.TextBox5.Text = "TextBox5";
         this.TextBox5.Location = new System.Drawing.Point(128, 76);
         this.TextBox5.Size = new System.Drawing.Size(120, 23);
         this.TextBox5.TabIndex = 5;
         //
         // TextBox6
         //
-        this.TextBox6.Text = "TextBox6";
         this.TextBox6.Location = new System.Drawing.Point(128, 120);
         this.TextBox6.Size = new System.Drawing.Size(120, 23);
         this.TextBox6.TabIndex = 6;
@@ -104,9 +103,7 @@ partial class Pood
         // ComboBox7
         //
         this.ComboBox7.ItemHeight = 15;
-        this.ComboBox7.SelectedText = "ComboBox7";
-        this.ComboBox7.SelectionLength = 9;
-        this.ComboBox7.Text = "ComboBox7";
+        this.ComboBox7.Text = "Vali";
         this.ComboBox7.Location = new System.Drawing.Point(128, 164);
         this.ComboBox7.TabIndex = 7;
         //
@@ -130,7 +127,7 @@ partial class Pood
         this.Button9.Location = new System.Drawing.Point(136, 208);
         this.Button9.Size = new System.Drawing.Size(112, 30);
         this.Button9.TabIndex = 9;
-        //this.Button9.Click += new System.EventHandler(Button9_Click);
+        this.Button9.Click += new System.EventHandler(Button9_Click);
         //
         // Button10
         //
@@ -141,14 +138,28 @@ partial class Pood
         this.Button10.Location = new System.Drawing.Point(256, 208);
         this.Button10.Size = new System.Drawing.Size(112, 30);
         this.Button10.TabIndex = 10;
-        //this.Button10.Click += new System.EventHandler(Button10_Click);
+        this.Button10.Click += new System.EventHandler(Button10_Click);
         //
         // DataGridView11
         //
+        this.DataGridView11.AllowUserToAddRows = false;
+        this.DataGridView11.AllowUserToDeleteRows = false;
+        this.DataGridView11.AllowUserToResizeColumns = true;
+        this.DataGridView11.AllowUserToResizeRows = false;
+        this.DataGridView11.ReadOnly = true;
         this.DataGridView11.Text = "DataGridView11";
         this.DataGridView11.Location = new System.Drawing.Point(16, 248);
         this.DataGridView11.Size = new System.Drawing.Size(484, 164);
         this.DataGridView11.TabIndex = 11;
+        this.DataGridView11.AutoGenerateColumns = false;
+        this.DataGridView11.RowTemplate.Height = 60;
+        LooVeerud();
+        this.DataGridView11.Columns["Lisatud"].Width = 120;
+        DataGridView11.Columns["PiltPreview"].Width = 80;
+        DataGridView11.Columns["Kogus"].Width = 50;
+        DataGridView11.Columns["Hind"].Width = 50;
+        DataGridView11.Columns["Id"].Width = 50;
+
         //
         // PictureBox13
         //
@@ -156,6 +167,17 @@ partial class Pood
         this.PictureBox13.Text = "PictureBox13";
         this.PictureBox13.Location = new System.Drawing.Point(260, 32);
         this.PictureBox13.Size = new System.Drawing.Size(240, 156);
+        //
+        // Button13
+        //
+        this.Button13.BackColor = System.Drawing.Color.LightSalmon;
+        this.Button13.Text = "Vali pilt";
+        this.Button13.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+        this.Button13.ForeColor = System.Drawing.Color.White;
+        this.Button13.Location = new System.Drawing.Point(376, 208);
+        this.Button13.Size = new System.Drawing.Size(112, 30);
+        this.Button13.TabIndex = 13;
+        this.Button13.Click += new System.EventHandler(Button13_Click);
         //
         // form
         //
@@ -174,12 +196,80 @@ partial class Pood
         this.Controls.Add(this.Button10);
         this.Controls.Add(this.DataGridView11);
         this.Controls.Add(this.PictureBox13);
+        this.Controls.Add(this.Button13);
         ((System.ComponentModel.ISupportInitialize)(this.DataGridView11)).EndInit();
         ((System.ComponentModel.ISupportInitialize)(this.PictureBox13)).EndInit();
         this.ResumeLayout(false);
+        //
+        // ComboLang
+        //
+        this.ComboLang.DropDownStyle = ComboBoxStyle.DropDownList;
+        this.ComboLang.Items.AddRange(new object[] { "ET", "EN" });
+        this.ComboLang.SelectedIndex = 0; // default: Estonian
+        this.ComboLang.Location = new System.Drawing.Point(430, 5);
+        this.ComboLang.Size = new System.Drawing.Size(60, 23);
+        this.ComboLang.SelectedIndexChanged += new System.EventHandler(ComboLang_SelectedIndexChanged);
+
+        this.Controls.Add(this.ComboLang);
     }
 
+
+
     #endregion
+
+    private void LooVeerud()
+    {
+        DataGridView11.Columns.Clear();
+
+        DataGridView11.Columns.Add(new DataGridViewTextBoxColumn()
+        {
+            Name = "Id",
+            HeaderText = "ID"
+        });
+
+        DataGridView11.Columns.Add(new DataGridViewTextBoxColumn()
+        {
+            Name = "ToodeNimetus",
+            HeaderText = "Nimetus"
+        });
+
+        DataGridView11.Columns.Add(new DataGridViewTextBoxColumn()
+        {
+            Name = "Kogus",
+            HeaderText = "Kogus"
+        });
+
+        DataGridView11.Columns.Add(new DataGridViewTextBoxColumn()
+        {
+            Name = "Hind",
+            HeaderText = "Hind"
+        });
+
+        DataGridView11.Columns.Add(new DataGridViewTextBoxColumn()
+        {
+            Name = "Kategooria",
+            HeaderText = "Kategooria"
+        });
+
+        DataGridViewImageColumn imgCol = new DataGridViewImageColumn()
+        {
+            Name = "PiltPreview",
+            HeaderText = "Pilt",
+            ImageLayout = DataGridViewImageCellLayout.Zoom
+        };
+        DataGridView11.Columns.Add(imgCol);
+
+        DataGridView11.Columns.Add(new DataGridViewTextBoxColumn()
+        {
+            Name = "Lisatud",
+            HeaderText = "Lisatud"
+        });
+
+        foreach (DataGridViewColumn col in DataGridView11.Columns)
+        {
+            col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+        }
+    }
 
     private System.Windows.Forms.Label Label0;
     private System.Windows.Forms.Label Label1;
@@ -194,6 +284,8 @@ partial class Pood
     private System.Windows.Forms.Button Button10;
     private System.Windows.Forms.DataGridView DataGridView11;
     private System.Windows.Forms.PictureBox PictureBox13;
+    private System.Windows.Forms.Button Button13;
+    private System.Windows.Forms.ComboBox ComboLang;
 }
 
 // private void Button8_Click(System.Object? sender, System.EventArgs e)
@@ -207,6 +299,11 @@ partial class Pood
 // }
 
 // private void Button10_Click(System.Object? sender, System.EventArgs e)
+// {
+// 
+// }
+
+// private void Button13_Click(System.Object? sender, System.EventArgs e)
 // {
 // 
 // }
--- Tund9/Pood.cs
+++ Tund9/Pood.cs
@@ -5,9 +5,27 @@ namespace Tund9;
 public partial class Pood : Form
 {
     private readonly TootedContext _db;
+    private readonly string configPath = Path.Combine(Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory), "config.txt");
     public Pood()
     {
         InitializeComponent();
+        if (File.Exists(configPath))
+        {
+            string lang = File.ReadAllText(configPath).Trim();
+            if (lang == "EN" || lang == "ET")
+                ComboLang.SelectedItem = lang;
+        }
+        else
+        {
+            ComboLang.SelectedItem = "ET";
+        }
+        SetLanguage(ComboLang.SelectedItem.ToString());
+
+        TextBox4.TextChanged += (_, __) => UuendaNupud();
+        TextBox5.TextChanged += (_, __) => UuendaNupud();
+        TextBox6.TextChanged += (_, __) => UuendaNupud();
+        ComboBox7.SelectedIndexChanged += (_, __) => UuendaNupud();
+        DataGridView11.SelectionChanged += (_, __) => UuendaNupud();
 
         _db = new TootedContext();
         LaeKategoorariad();
@@ -16,19 +34,41 @@ public partial class Pood : Form
         {
             context.EnsureCreated();
         }
+        UuendaNupud();
     }
     private void LaeTooted()
     {
-        DataGridView11.DataSource = _db.Tooted.Include(t => t.Kategooria).Select(t => new
+        string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+        string projectDir = Path.GetFullPath(Path.Combine(baseDir, "..", "..", ".."));
+        string imagesFolder = Path.Combine(projectDir, "Images");
+
+        var list = _db.Tooted.Include(t => t.Kategooria).ToList();
+
+        DataGridView11.Rows.Clear();
+
+        foreach (var t in list)
         {
-            t.Id,
-            t.ToodeNimetus,
-            t.Kogus,
-            t.Hind,
-            Kategooria = t.Kategooria.KategooriaNimetus,
-            t.Pilt,
-            t.Lisatud
-        }).ToList();
+            var imgPath = Path.Combine(imagesFolder, t.Pilt ?? "default.png");
+
+            Image img = null;
+            if (File.Exists(imgPath))
+            {
+                using (var temp = Image.FromFile(imgPath))
+                {
+                    img = new Bitmap(temp);
+                }
+            }
+
+            DataGridView11.Rows.Add(
+                t.Id,
+                t.ToodeNimetus,
+                t.Kogus,
+                t.Hind,
+                t.Kategooria.KategooriaNimetus,
+                img,
+                t.Lisatud
+            );
+        }
     }
     private void LaeKategoorariad()
     {
@@ -55,8 +95,18 @@ public partial class Pood : Form
             KategooriaId = (int)ComboBox7.SelectedValue,
             Lisatud = DateTime.Now
         };
-        string imagesFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images");
+        string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+        string projectDir = Path.GetFullPath(Path.Combine(baseDir, "..", "..", ".."));
+
+        string imagesFolder = Path.Combine(projectDir, "Images");
+
+        if (!Directory.Exists(imagesFolder))
+        {
+            Directory.CreateDirectory(imagesFolder);
+        }
+
         string fileName;
+
         if (string.IsNullOrEmpty(valitudPilt))
         {
             fileName = "default.png";
@@ -67,10 +117,220 @@ public partial class Pood : Form
             string destPath = Path.Combine(imagesFolder, fileName);
             File.Copy(valitudPilt, destPath, true);
         }
+
         uus.Pilt = fileName;
         _db.Tooted.Add(uus);
         _db.SaveChanges();
         LaeTooted();
-        //PuhastaVorm();
+        PuhastaVorm();
+    }
+    private void Button13_Click(System.Object? sender, System.EventArgs e)
+    {
+        OpenFileDialog dlg = new OpenFileDialog();
+        dlg.Filter = "Pildi failid|*.jpg;*.png;*.jpeg;*.bmp";
+        if (dlg.ShowDialog() == DialogResult.OK)
+        {
+            valitudPilt = dlg.FileName;
+            PictureBox13.Image = Image.FromFile(valitudPilt);
+            PictureBox13.SizeMode = PictureBoxSizeMode.Zoom;
+            UuendaNupud();
+        }
+    }
+    private void PuhastaVorm()
+    {
+        TextBox4.Clear();
+        TextBox5.Clear();
+        TextBox6.Clear();
+        ComboBox7.SelectedIndex = -1;
+        PictureBox13.Image = null;
+        valitudPilt = string.Empty;
+    }
+    private void Button9_Click(object? sender, EventArgs e)
+    {
+        if (DataGridView11.SelectedRows.Count == 0)
+        {
+            MessageBox.Show("Vali rida.");
+            return;
+        }
+
+        int id = (int)DataGridView11.SelectedRows[0].Cells["Id"].Value;
+        var toode = _db.Tooted.Find(id);
+
+        if (TextBox4.Text != "" && TextBox4.Text != toode.ToodeNimetus)
+            toode.ToodeNimetus = TextBox4.Text;
+
+        if (TextBox5.Text != "" && int.TryParse(TextBox5.Text, out int kogus)
+            && kogus != toode.Kogus)
+            toode.Kogus = kogus;
+
+        if (TextBox6.Text != "" && double.TryParse(TextBox6.Text, out double hind)
+            && hind != toode.Hind)
+            toode.Hind = hind;
+
+        if (ComboBox7.SelectedItem != null &&
+            (int)ComboBox7.SelectedValue != toode.KategooriaId)
+            toode.KategooriaId = (int)ComboBox7.SelectedValue;
+
+        if (!string.IsNullOrEmpty(valitudPilt))
+        {
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            string projectDir = Path.GetFullPath(Path.Combine(baseDir, "..", "..", ".."));
+            string imagesFolder = Path.Combine(projectDir, "Images");
+
+            Directory.CreateDirectory(imagesFolder);
+
+            string fileName = Path.GetFileName(valitudPilt);
+            File.Copy(valitudPilt, Path.Combine(imagesFolder, fileName), true);
+            toode.Pilt = fileName;
+        }
+
+        _db.SaveChanges();
+        LaeTooted();
+        UuendaNupud();
+    }
+    private void Button10_Click(object? sender, EventArgs e)
+    {
+        if (DataGridView11.SelectedRows.Count == 0)
+        {
+            MessageBox.Show("Palun vali toode, mida soovid kustutada.");
+            return;
+        }
+
+        string toodeNimetus = DataGridView11.SelectedRows[0]
+            .Cells["ToodeNimetus"].Value.ToString();
+
+        DialogResult vastus = MessageBox.Show(
+            $"Kas oled kindel, et soovid toote '{toodeNimetus}' kustutada?",
+            "Kinnita kustutamine",
+            MessageBoxButtons.YesNo);
+
+        if (vastus != DialogResult.Yes)
+            return;
+
+        try
+        {
+            int id = (int)DataGridView11.SelectedRows[0].Cells["Id"].Value;
+            var toode = _db.Tooted.Find(id);
+
+            if (toode != null)
+            {
+                // build image path
+                string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+                string projectDir = Path.GetFullPath(Path.Combine(baseDir, "..", "..", ".."));
+                string imagesFolder = Path.Combine(projectDir, "Images");
+
+                string imgPath = Path.Combine(imagesFolder, toode.Pilt ?? "");
+
+                // delete DB record
+                _db.Tooted.Remove(toode);
+                _db.SaveChanges();
+
+                // delete image file if not default and file exists
+                if (toode.Pilt != "default.png" &&
+                    File.Exists(imgPath))
+                {
+                    try
+                    {
+                        File.Delete(imgPath);
+                    }
+                    catch
+                    {
+                        // ignore, no crash
+                    }
+                }
+
+                LaeTooted();
+                PuhastaVorm();
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Tekkis viga: {ex.Message}");
+        }
+    }
+    private void UuendaNupud()
+    {
+        // Lisa (ADD)
+        Button8.Enabled =
+            !string.IsNullOrWhiteSpace(TextBox4.Text) ||
+            !string.IsNullOrWhiteSpace(TextBox5.Text) ||
+            !string.IsNullOrWhiteSpace(TextBox6.Text) ||
+            ComboBox7.SelectedItem != null ||
+            !string.IsNullOrEmpty(valitudPilt);
+
+        // Delete
+        bool rowSelected = DataGridView11.SelectedRows.Count > 0;
+        Button10.Enabled = rowSelected;
+
+        // Update
+        if (!rowSelected)
+        {
+            Button9.Enabled = false;
+            return;
+        }
+
+        var r = DataGridView11.SelectedRows[0];
+
+        bool changed =
+            (TextBox4.Text != "" &&
+             TextBox4.Text != r.Cells["ToodeNimetus"].Value.ToString()) ||
+            (TextBox5.Text != "" &&
+             TextBox5.Text != r.Cells["Kogus"].Value.ToString()) ||
+            (TextBox6.Text != "" &&
+             TextBox6.Text != r.Cells["Hind"].Value.ToString()) ||
+            (ComboBox7.SelectedItem != null &&
+             ComboBox7.Text != r.Cells["Kategooria"].Value.ToString()) ||
+            !string.IsNullOrEmpty(valitudPilt);
+
+        Button9.Enabled = changed;
+    }
+    private void SetLanguage(string lang)
+    {
+        if (lang == "EN")
+        {
+            Label0.Text = "Product name";
+            Label1.Text = "Product quantity";
+            Label2.Text = "Product price";
+            Label3.Text = "Category";
+
+            Button8.Text = "Add";
+            Button9.Text = "Update";
+            Button10.Text = "Delete";
+            Button13.Text = "Choose image";
+
+            this.Text = "Store";
+        }
+        else // ET (default)
+        {
+            Label0.Text = "Toode nimetus";
+            Label1.Text = "Toode kogus";
+            Label2.Text = "Toode hind";
+            Label3.Text = "Kategooria";
+
+            Button8.Text = "Lisa";
+            Button9.Text = "Uuenda";
+            Button10.Text = "Kustuta";
+            Button13.Text = "Vali pilt";
+
+            this.Text = "Pood";
+        }
+    }
+    private void ComboLang_SelectedIndexChanged(object? sender, EventArgs e)
+    {
+        string lang = ComboLang.SelectedItem.ToString();
+        SetLanguage(lang);
+    }
+    protected override void OnFormClosing(FormClosingEventArgs e)
+    {
+        base.OnFormClosing(e);
+
+        try
+        {
+            File.WriteAllText(configPath, ComboLang.SelectedItem?.ToString() ?? "ET");
+        }
+        catch
+        {
+            // ignore errors
+        }
     }
 }
